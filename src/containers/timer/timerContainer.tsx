28b95909f42992f5c089061d11bf81a3cbd96059
xoxo30 <jounis3668@gmail.com>
2021-09-07 23:32:01 +0900

    .

--- src/containers/timer/timerContainer.tsx
+++ src/containers/timer/timerContainer.tsx
@@ -5,6 +5,11 @@ import Analog from "../../components/analog";
 function TimerContainer() {
   return (
     <>
+      <div>
+        <a href="http://naver.me/GyeastIj">
+          파일 링크 http://naver.me/GyeastIj
+        </a>
+      </div>
       <Analog />
     </>
   );
